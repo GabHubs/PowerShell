743cf07eff1a8b1956f168781394a7a29788384d
gabhubert91 <gabriel_hubert@hotmail.com>
2022-05-24 23:15:48 -0400

    Update Clean-Disk-Remote.psm1
    - Correction au niveau de la transcription

--- Clean-Disk-Remote.psm1
+++ Clean-Disk-Remote.psm1
@@ -1,7 +1,7 @@
 function Clean-Disk-Remote {
 <#
 .SYNOPSIS
-    Ce script va nettoyer différents cache de tous les profils des utilisateurs d'une machine à distance.
+    Ce script va nettoyer différents cache de tous les profils des utilisateurs d'une machine à distance. Ce script a été conçu dans un environnement où le protocole WinRM est bloqué.
  
 .NOTES
     Name: Clean-Disk-Remote
@@ -10,66 +10,67 @@ function Clean-Disk-Remote {
     DateCreated: 2020-Aug-5
  
  .DESCRIPTION
-    Ce script liste l'ensemble des dossiers utilisateur à l'aide de l'accès à distance (C$) et, avec l'aide d'un tableau, efface les différentes caches dans ceux-ci. Ce script est une alternative au "Nettoyage de disque" présent dans les versions plus récente de Windows
+    Ce script liste l'ensemble des dossiers utilisateur
 
 .LINK
-    https://github.com/gabhubert91/PowerShell  (PRIVATE)
+    https://github.com/gabhubert91/PowerShell
 
 .EXAMPLE
     Clean-Disk-Remote -Computer NomDeLaMachine
   
-.PARAMETER computer
-    Preciser le nom de la machine où le nettoyage doit s'effectuer
+.PARAMETER Computer
+    Preciser le nom de la machine ou le nettoyage doit s'effectuer
 #>
 
 param (
         [Parameter(Mandatory=$true)]
-        [string]$computer
+        [string]$Computer
       )
-$test = Test-Path -path "\\$computer\c$"
+
+$test = Test-Path -path "\\$Computer\c$"
 
 If($test -eq $true)
 {
 
     # On liste les utilisateurs qui ont ouvert une session sur la machine
-    $tableau_user = dir \\$computer\c$\Users 
+    $tableau_user = dir \\$Computer\c$\Users
 
     # Si la liste existe, on peut continuer
     if ($tableau_user.count -gt 0) {
 
-       Start-Transcript -Path "c:\users\$env:USERNAME\desktop\cleanlog-$computer.txt" #Logs
+       Start-Transcript -Path "c:\users\$env:USERNAME\desktop\cleanlog-$Computer.txt" #Logs
        foreach ($user in $tableau_user){
             
                 #Firefox
-                Remove-Item -path \\$computer\c$\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\cache\* -Recurse -Force -EA SilentlyContinue -Verbose  
-                Remove-Item -path \\$computer\c$\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\cache\*.* -Recurse -Force -EA SilentlyContinue -Verbose  
-	            Remove-Item -path \\$computer\c$\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\cache2\entries\*.* -Recurse -Force -EA SilentlyContinue -Verbose  
-                Remove-Item -path \\$computer\c$\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\thumbnails\* -Recurse -Force -EA SilentlyContinue -Verbose  
-                Remove-Item -path \\$computer\c$\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\cookies.sqlite -Recurse -Force -EA SilentlyContinue -Verbose  
-                Remove-Item -path \\$computer\c$\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\webappsstore.sqlite -Recurse -Force -EA SilentlyContinue -Verbose  
-                Remove-Item -path \\$computer\c$\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\chromeappsstore.sqlite -Recurse -Force -EA SilentlyContinue -Verbose  
+                Remove-Item -path \\$Computer\c$\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\cache\* -Recurse -Force -EA SilentlyContinue -Verbose  
+                Remove-Item -path \\$Computer\c$\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\cache\*.* -Recurse -Force -EA SilentlyContinue -Verbose  
+	            Remove-Item -path \\$Computer\c$\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\cache2\entries\*.* -Recurse -Force -EA SilentlyContinue -Verbose  
+                Remove-Item -path \\$Computer\c$\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\thumbnails\* -Recurse -Force -EA SilentlyContinue -Verbose  
+                Remove-Item -path \\$Computer\c$\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\cookies.sqlite -Recurse -Force -EA SilentlyContinue -Verbose  
+                Remove-Item -path \\$Computer\c$\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\webappsstore.sqlite -Recurse -Force -EA SilentlyContinue -Verbose  
+                Remove-Item -path \\$Computer\c$\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\chromeappsstore.sqlite -Recurse -Force -EA SilentlyContinue -Verbose  
                 #Chrome
-                Remove-Item -path "\\$computer\c$\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\Cache\*" -Recurse -Force -EA SilentlyContinue -Verbose  
-                Remove-Item -path "\\$computer\c$\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\Cache2\entries\*" -Recurse -Force -EA SilentlyContinue -Verbose  
-                Remove-Item -path "\\$computer\c$\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\Cookies" -Recurse -Force -EA SilentlyContinue -Verbose  
-                Remove-Item -path "\\$computer\c$\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\Media Cache" -Recurse -Force -EA SilentlyContinue -Verbose  
-                Remove-Item -path "\\$computer\c$\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\Cookies-Journal" -Recurse -Force -EA SilentlyContinue -Verbose  
+                Remove-Item -path "\\$Computer\c$\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\Cache\*" -Recurse -Force -EA SilentlyContinue -Verbose  
+                Remove-Item -path "\\$Computer\c$\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\Cache2\entries\*" -Recurse -Force -EA SilentlyContinue -Verbose  
+                Remove-Item -path "\\$Computer\c$\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\Cookies" -Recurse -Force -EA SilentlyContinue -Verbose  
+                Remove-Item -path "\\$Computer\c$\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\Media Cache" -Recurse -Force -EA SilentlyContinue -Verbose  
+                Remove-Item -path "\\$Computer\c$\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\Cookies-Journal" -Recurse -Force -EA SilentlyContinue -Verbose  
                 #Edge
-                Remove-Item -path "\\$computer\c$\Users\$($_.Name)\AppData\Local\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\*" -Recurse -Force -EA SilentlyContinue -Verbose  
+                Remove-Item -path "\\$Computer\c$\Users\$($_.Name)\AppData\Local\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\*" -Recurse -Force -EA SilentlyContinue -Verbose  
                 #IE
-                Remove-Item -path "\\$computer\c$\Users\$($_.Name)\AppData\Local\Microsoft\Windows\Temporary Internet Files\*" -Recurse -Force -EA SilentlyContinue -Verbose  
-	            Remove-Item -path "\\$computer\c$\Users\$($_.Name)\AppData\Local\Microsoft\Windows\WER\*" -Recurse -Force -EA SilentlyContinue -Verbose  
-	            Remove-Item -path "\\$computer\c$\Users\$($_.Name)\AppData\Local\Temp\*" -Recurse -Force -EA SilentlyContinue -Verbose
-                Remove-Item -Path "\\$computer\c$\Users\$($_.Name)\AppData\Local\Microsoft\Windows\INetCache\IE\*" -Recurse -Force -Verbose -EA SilentlyContinue
-                Remove-Item -Path "\\$computer\c$\Users\$($_.Name)\AppData\Local\Microsoft\Windows\INetCache\IE\*" -Recurse -Force -Verbose -EA SilentlyContinue # Il semble resté des fichiers après la première suppression  
+                Remove-Item -path "\\$Computer\c$\Users\$($_.Name)\AppData\Local\Microsoft\Windows\Temporary Internet Files\*" -Recurse -Force -EA SilentlyContinue -Verbose  
+	            Remove-Item -path "\\$Computer\c$\Users\$($_.Name)\AppData\Local\Microsoft\Windows\WER\*" -Recurse -Force -EA SilentlyContinue -Verbose  
+	            Remove-Item -path "\\$Computer\c$\Users\$($_.Name)\AppData\Local\Temp\*" -Recurse -Force -EA SilentlyContinue -Verbose
+                Remove-Item -Path "\\$Computer\c$\Users\$($_.Name)\AppData\Local\Microsoft\Windows\INetCache\IE\*" -Recurse -Force -Verbose -EA SilentlyContinue
+                Remove-Item -Path "\\$Computer\c$\Users\$($_.Name)\AppData\Local\Microsoft\Windows\INetCache\IE\*" -Recurse -Force -Verbose -EA SilentlyContinue # Il semble resté des fichiers après la première suppression  
 	            #Windows
-                Remove-Item -path "\\$computer\c$\Windows\Temp\*" -Recurse -Force -EA SilentlyContinue -Verbose  
-	            Remove-Item -path "\\$computer\c$\`$recycle.bin\" -Recurse -Force -EA SilentlyContinue -Verbose  
+                Remove-Item -path "\\$Computer\c$\Windows\Temp\*" -Recurse -Force -EA SilentlyContinue -Verbose  
+	            Remove-Item -path "\\$Computer\c$\`$recycle.bin\" -Recurse -Force -EA SilentlyContinue -Verbose  
                 #Cache local de l'utilisateur
-                Remove-Item -path "\\$computer\c$\Users\$($_.Name)\AppData\Local\Temp\*" -Recurse -Force -EA SilentlyContinue -Verbose  
-                Remove-Item -path "\\$computer\c$\Users\$($_.Name)\downloads\*" -Recurse -Force -EA SilentlyContinue -Verbose  
+                Remove-Item -path "\\$Computer\c$\Users\$($_.Name)\AppData\Local\Temp\*" -Recurse -Force -EA SilentlyContinue -Verbose  
+                Remove-Item -path "\\$Computer\c$\Users\$($_.Name)\downloads\*" -Recurse -Force -EA SilentlyContinue -Verbose  
                 #.jmap
-                Remove-Item -path "\\$computer\c$\Users\$($_.Name)\.jmap" -Recurse -Force -EA SilentlyContinue -Verbose  
+                Remove-Item -path "\\$Computer\c$\Users\$($_.Name)\.jmap" -Recurse -Force -EA SilentlyContinue -Verbose  
                 $count = $count + 1
             
                 }
@@ -77,16 +78,17 @@ If($test -eq $true)
                 Write-Host "`n$count profils ont été nettoyés `n" -ForegroundColor Yellow
 
                 #Vidage du cache de SCCM
-                Remove-Item -path "\\$computer\c$\windows\ccmcache\*" -Recurse -Force -EA SilentlyContinue -Verbose  
+                Remove-Item -path "\\$Computer\c$\windows\ccmcache\*" -Recurse -Force -EA SilentlyContinue -Verbose  
             
                 #MemoryDump
-                Remove-Item -path "\\$computer\c$\Windows\System32\memory.dmp" -Recurse -Force -EA SilentlyContinue -Verbose  
+                Remove-Item -path "\\$Computer\c$\Windows\System32\memory.dmp" -Recurse -Force -EA SilentlyContinue -Verbose  
             
 
                 #Résultat
                 $nb_user = $tableau_user.Count
-                Write-Host "`nNettoyage réussi sur $nb_user utilisateurs de l'ordinateur $computer. Un fichier log a été créé sur votre bureau`n"-ForegroundColor Yellow
+                Write-Host "`nNettoyage réussi sur $nb_user utilisateurs de l'ordinateur $Computer. Un fichier log a été créé sur votre bureau`n"-ForegroundColor Yellow
                 Pause
+                cls
                 Stop-Transcript
             } 
     
